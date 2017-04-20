7b1f510c8b78799063ce2d506d8dbfe59cdd96f3
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-20 14:51:27 +0200

    added 'read x images from dir' msg

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv) {
-    vector<pair<Mat,string>> images = ImgReader().read_from_dir("data");
+    string dir = "data";
+    vector<pair<Mat,string>> images = ImgReader().read_from_dir(dir);
+    cout << "read " << images.size() << " images from " << dir << endl;
     for(pair<Mat,string> img : images){
         imshow(img.second, img.first);
     }
