cd90954f60c665daa1f9df8553cfcca572dc1e31
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-20 21:53:29 +0200

    fixed error in calc_bin in histogramm

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
     string dir = "data";
     vector<string> imgnames = ImgReader().get_filenames_from_dir(dir);
     vector<Mat> imgs = ImgReader().read_imgs_from_filenames(imgnames);
-    cout << "read " << imgs.size() << " images from " << dir << endl;
+    cout << "read images: " << endl << imgnames << endl;
 
     vector<Mat> hsv_imgs = HSVConverter().convert_all(imgs);
     cout << "converted images to hsv space" << endl;
@@ -43,17 +43,31 @@ int main(int argc, char **argv) {
     }
     cout << "maximum hsv elements " << maxs << endl;
 
-    Vec3i nbins(16, 3, 3);
-    Histogramm hist(nbins, HSVConverter::hsv_range);
-    for(int i = 0; i < HSVConverter::hsv_range[0]; i++)
-        for(float j = 0; j < HSVConverter::hsv_range[1]; j += 0.01)
-            for(float k = 0; k < HSVConverter::hsv_range[2]; k += 0.01)
-                hist.increment_bin(Vec3f(i,j,k));
+    //Vec3f err(340, 1, 0.0117647);
+    //cout << "binnn " << Histogramm (Vec3i(16, 3, 3), HSVConverter::hsv_range).calc_bin(err) << endl;
 
-    cout << "histogramm: " << endl;
-    for(int i = 0; i < hist.get_bins().size(); i++){
-        cout << i << ": " << hist.get_bins()[i] << " (" << hist.calc_repr(i) << ")" << endl;
+    Vec3i nbins(16, 3, 3);
+    vector<Histogramm> histograms;
+    histograms.reserve(imgs.size());
+    for(Mat hsv_img : hsv_imgs){
+        cout << "a" << endl;
+        Histogramm hist(nbins, HSVConverter::hsv_range);
+        cout << "b" << endl;
+        for(int i = 0; i < hsv_img.rows; i++){
+            for(int j = 0; j < hsv_img.cols; j++){
+                hist.increment_bin(hsv_img.at<Vec3f>(i, j));
+            }
+        }
+        histograms.push_back(hist);
+        cout << "c" << endl;
     }
+
+    cout << "histograms of images: " << endl;
+    for(int i = 0; i < histograms.size(); i++){
+        cout << i << ": " << histograms[i].get_bins() << endl;
+    }
+
+
 
     for(int i = 0; i < imgs.size(); i++){
         imshow(imgnames[i], imgs[i]);
--- src/Histogramm.cpp
+++ src/Histogramm.cpp
@@ -18,9 +18,9 @@ const vector<int>& Histogramm::get_bins(){
 }
 
 int Histogramm::calc_bin(const Vec3f &color){
-    int s0 = nbins[1] * nbins[2] * floor(color[0] * nbins[0] / range[0]);
-    int s1 = nbins[2] * floor(color[1] * nbins[1] / range[1]);
-    int s2 = floor(color[2] * nbins[2] / range[2]);
+    int s0 = nbins[1] * nbins[2] * color[0] * floor(nbins[0] / range[0]);
+    int s1 = nbins[2] * color[1] * floor(nbins[1] / range[1]);
+    int s2 = color[2] *floor(nbins[2] / range[2]);
     return s0 + s1 + s2;
 }
 
