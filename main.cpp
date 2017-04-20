29d63bdcb988367cb8c8c45208a20dea49899a32
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-20 15:43:01 +0200

    added hsv transformation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include "ImgReader.h"
 
 using namespace std;
@@ -11,6 +10,23 @@ int main(int argc, char **argv) {
     vector<string> imgnames = ImgReader().get_filenames_from_dir(dir);
     vector<Mat> imgs = ImgReader().read_imgs_from_filenames(imgnames);
     cout << "read " << imgs.size() << " images from " << dir << endl;
+
+    vector<Mat> hsv_imgs;
+    for(Mat img : imgs){
+        Mat hsv_img;
+        cvtColor(img, hsv_img, CV_BGR2HSV);
+        hsv_imgs.push_back(hsv_img);
+    }
+    cout << "converted images to hsv space" << endl;
+
+    for(Mat hsv_img : hsv_imgs){
+        for(int i = 0; i < hsv_img.rows; i++){
+            for(int j = 0; j < hsv_img.cols; j++){
+               cout << hsv_img.at<Vec3b>(i, j) << endl;
+            }
+        }
+    }
+
     for(int i = 0; i < imgs.size(); i++){
         imshow(imgnames[i], imgs[i]);
     }
