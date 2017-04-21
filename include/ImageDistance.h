34b7c92dd7f38840c67fd348324b6f3e4d6bb99b
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-21 14:34:41 +0200

    added image distance interface

---
+++ include/ImageDistance.h
@@ -0,0 +1,12 @@
+#ifndef IMAGEDISTANCE_H
+#define IMAGEDISTANCE_H
+
+#include <opencv2/opencv.hpp>
+
+class ImageDistance {
+public:
+    virtual double calc(cv::Mat hist1, cv::Mat hist2) = 0;
+    virtual ~ImageDistance() {}
+};
+
+#endif // IMAGEDISTANCE_H
