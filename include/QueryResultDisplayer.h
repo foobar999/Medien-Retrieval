78a46f2b84911516a2f5a46a6ce810854b5d8caf
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-24 14:37:40 +0200

    refactored query results displayer

--- include/QueryResultDisplayer.h
+++ include/QueryResultDisplayer.h
@@ -8,7 +8,8 @@ class QueryResultDisplayer {
 public:
     void display(cv::Mat query_img, const std::vector<std::vector<cv::Mat>> &results, std::string winname,
                  cv::Size thumb_size = cv::Size(100,100));
-
+protected:
+    cv::Mat create_query_img_thumb_row(cv::Mat query_img, cv::Size thumb_size, int num_padding_thumbs);
 };
 
 #endif // QUERYRESULTDISPLAYER_H
--- src/QueryResultDisplayer.cpp
+++ src/QueryResultDisplayer.cpp
@@ -4,29 +4,8 @@ using namespace cv;
 using namespace std;
 
 void QueryResultDisplayer::display(Mat query_img, const vector<vector<Mat>> &results, string winname, Size thumb_size) {
-    /*
-    Mat query_thumb;
-    cv::resize(query_img, query_thumb, thumb_size);
-    vector<Mat> results_thumbs(results.size());
-    for(int i = 0; i < results_thumbs.size(); i++) {
-        cv::resize(results[i], results_thumbs[i], thumb_size);
-    }
-    // construct displayed img with 2 rows:
-    // upper row: thumbnail of query img + padding
-    // lower row: thumbnails of most similar images, sorted
-    Mat lower_thumb_row, upper_thumb_row;
-    hconcat(results_thumbs, lower_thumb_row);
-    Mat query_thumb_padding(thumb_size.height, thumb_size.width*(results.size()-1), CV_8UC3, Scalar::all(0));
-    hconcat(query_thumb, query_thumb_padding, upper_thumb_row);
-    Mat displayed_img;
-    vconcat(upper_thumb_row, lower_thumb_row, displayed_img);
-    */
-
-    Mat query_thumb;
-    cv::resize(query_img, query_thumb, thumb_size);
-    Mat query_thumb_padding(thumb_size.height, thumb_size.width*(results[0].size()-1), CV_8UC3, Scalar::all(0));
-    Mat current_res;
-    hconcat(query_thumb, query_thumb_padding, current_res);
+    assert(results.size() > 0);
+    Mat current_res = create_query_img_thumb_row(query_img, thumb_size, results[0].size()-1);
     for(const vector<Mat> &result : results){
         vector<Mat> result_thumbs(result.size());
         for(int i = 0; i < result_thumbs.size(); i++) {
@@ -41,3 +20,13 @@ void QueryResultDisplayer::display(Mat query_img, const vector<vector<Mat>> &res
     waitKey();
     //cvDestroyWindow(winname.c_str());
 }
+
+Mat QueryResultDisplayer::create_query_img_thumb_row(Mat query_img, Size thumb_size, int num_padding_thumbs){
+    Mat query_thumb;
+    resize(query_img, query_thumb, thumb_size);
+    Mat query_thumb_padding(thumb_size.height, thumb_size.width * num_padding_thumbs, CV_8UC3, Scalar::all(0));
+    Mat query_thumb_row;
+    hconcat(query_thumb, query_thumb_padding, query_thumb_row);
+    return query_thumb_row;
+}
+
