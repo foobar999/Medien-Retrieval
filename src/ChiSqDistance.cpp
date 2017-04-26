72c176eacb3ee7df711e91deb502fd070c76f314
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-26 20:23:09 +0200

    set CHISQR to CHISQR_ALT (correct one)

--- src/ChiSqDistance.cpp
+++ src/ChiSqDistance.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 using namespace std;
 
 double ChiSqDistance::calc(const ImageData &dat1, const ImageData &dat2){
-    return compareHist(dat1.abs_hist, dat2.abs_hist, HISTCMP_CHISQR);
+    return compareHist(dat1.norm_hist, dat2.norm_hist, HISTCMP_CHISQR_ALT);
 }
 
 string ChiSqDistance::get_class_name(){
